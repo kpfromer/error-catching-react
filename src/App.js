d925484eedcbe6dd63f80cca52ec7e1329b5ccda
Kyle Pfromer <kpfromer2@gmail.com>
2018-05-31 18:11:30 -0600

    Add error catching with React

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ import './App.css';
 import StudentForm from './components/StudentForm';
 
 class App extends Component {
+
+  state = {
+    hasError: false
+  };
+
+  componentDidCatch() {
+    this.setState({ hasError: true })
+  }
+
   render() {
+
+    if (this.state.hasError) {
+      return <h1>Oh no! Something went wrong!</h1>
+    }
+
     return (
       <div className="App">
         <header className="App-header">
